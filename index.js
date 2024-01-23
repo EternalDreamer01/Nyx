a9371822bf69c8ebb923613a8e6902b84136f2a5
Nightmare <nightmare@eternal>
2024-01-23 15:42:49 +0100

    style: HTML & JSON

--- index.js
+++ index.js
@@ -3,6 +3,27 @@
 import WhatsApp from 'whatsapp-web.js';
 import QRCcode from "qrcode-terminal"
 import express from "express";
+import { ServerResponse } from "http";
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+export const root = dirname(fileURLToPath(import.meta.url));
+
+
+ServerResponse.prototype.html = function (title, content) {
+	return this.send(`<!DOCTYPE html>
+	<html lang="en-UK">
+	<head>
+		<meta charset="UTF-8">
+		<link rel="stylesheet" type="text/css" href="/style.css" media="all" />
+		<title>${title}</title>
+	</head>
+	<body>
+		<h1>${title}</h1>
+		${content}
+	</body>
+	</html>`);
+}
 
 const { Client, LocalAuth, Contact } = WhatsApp;
 const client = new Client({
@@ -29,42 +50,73 @@ client.on('qr', (qr) => {
 	QRCcode.generate(qr, { small: true });
 });
 
-client.on('ready', async () => {
+const error = "This phone number is not registered on WhatsApp or does not have enough public information";
+
+client.on('ready', () => {
 	console.log('Client is ready!');
 
 	const app = express();
 	const port = 80;
 
+	app.get('/style.css', (_, res) => res.sendFile("style.css", { root }));
+	app.get('/', (_, res) => {
+		res.html(
+			"Venom",
+			`<p>Enter a phone number in url, in this format:</p>
+			<p style="font-weight:bold;">&lt;country code&gt;&lt;personal phone number&gt;</p>
+			<p style="text-decoration: underline; margin-top:80px;">Example:</p>
+			<table>
+				<tr>
+					<td>Country code</td>
+					<td>44 (United Kingdom)</td>
+				</tr>
+				<tr>
+					<td>Personal number</td>
+					<td>1234 123456</td>
+				</tr>
+			</table>
+			<p><a href="/441234123456">441234123456</a></p>`
+		);
+	});
 	app.get('/:phone', async (req, res) => {
 		try {
+			const contact = await client.getContactById(req.params.phone + "@c.us");
+			const [picture, number, about] = await Promise.all([
+				contact.getProfilePicUrl(),
+				contact.getFormattedNumber(),
+				contact.getAbout()
+			]);
+			if (contact.pushname === undefined && !picture && !about)
+				throw new Error("Not found");
 
-			if (!req.params.phone.endsWith("@c.us"))
-				req.params.phone += "@c.us";
-			const contact = await client.getContactById(req.params.phone);
-			console.log(contact);
-			res.send(
-				`<html>
-				<style>
-					body {
-						background:black;
-						margin:10
-					}
-					* {
-						color:white;
-					}
-				</style>
-				<body>
-					<img src="${await contact.getProfilePicUrl()}" />
-					<p>${await contact.getFormattedNumber()}</p>
-					<p>${contact.pushname}</p>
-					<p>${await contact.getAbout()}</p>
-				</body>
-				</html>`
-			);
+			if (req.query.json)
+				return res.json({
+					name: contact.pushname,
+					picture,
+					number,
+					about
+				});
+
+			let html = (picture !== undefined ? `<img src="${picture}" />` : "") + "<table>" +
+				Object.entries({
+					number,
+					about
+				})
+				.map(([field, content]) => (
+					`<tr>
+						<td>${field.charAt(0).toUpperCase() + field.slice(1)}</td>
+						<td>${content}</td>
+					</tr>`
+				)) +
+				"</table>";
+			res.html(contact.pushname || number, html);
 		}
-		catch (error) {
-			console.error(error);
-			res.send("Error");
+		catch (e) {
+			res.status(404);
+			if (req.query.json)
+				res.json({ error });
+			else
+				res.html("Not found", error);
 		}
 	});
 
